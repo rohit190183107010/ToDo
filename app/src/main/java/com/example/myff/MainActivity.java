ae912ec5890fbfb05bca1a978fed88b8b4d673a2
rohit.190183107010 <rohit.190183107010@gmail.com>
2023-04-08 12:10:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myff/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myff;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button button1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
